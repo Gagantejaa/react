e8b0b3a858c91e6856b36b144910da007d14432c
Gagantejaa <kada.gaganteja12@gmail.com>
2025-03-03 16:37:43 +0530

    Second Commit

---
+++ src/components/databinding/DataBinding.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+ 
+function CounterApp() {
+  const [count, setCount] = useState(0);
+ 
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+ 
+  return (
+<div>
+<h1>Counter: {count}</h1>
+<button onClick={increment}>Increment</button>
+<button onClick={decrement}>Decrement</button>
+</div>
+  );
+}
+ 
+export default CounterApp; 
+//This is the code done so Far ...!
