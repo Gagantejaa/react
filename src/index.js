0aee01f33ade3d7e15b7823b2c19dd20e0bc640f
Gagantejaa <kada.gaganteja12@gmail.com>
2025-03-03 16:34:11 +0530

    Initial commit to dev branch

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CounterApp from './components/databinding/DataBinding';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CounterApp />
   </React.StrictMode>
 );
 
