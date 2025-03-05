763b828f6753262ebd0a5c86236063a1c55c9f4a
Gagantejaa <kada.gaganteja12@gmail.com>
2025-03-05 17:46:58 +0530

    Dynamic and Ecom React

---
+++ src/myRouterApp/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./RouterApp.css";
+
+const About = () => {
+  return (
+    <div className="page-container">
+      <h1>About Page</h1>
+      <p>This is the about page where you can learn more about us.</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/myRouterApp/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./RouterApp.css";
+
+const Home = () => {
+  return (
+    <div className="page-container">
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the home page of our React Router example.</p>
+    </div>
+  );
+};
+
+export default Home;
